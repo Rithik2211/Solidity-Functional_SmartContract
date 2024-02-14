d80926a2825348059bf1aba2ceb660b365d9706f
Rithik2211 <rithikpranao22@gmail.com>
2024-02-14 22:54:44 +0530

    updated in the proxy contract

--- test/Proxy.js
+++ test/Proxy.js
@@ -12,32 +12,35 @@ describe("Proxy", function () {
     const Logic2 = await ethers.getContractFactory("Logic2");
     const logic2 = await Logic2.deploy();
 
-    return { proxy , logic1, logic2 };
+    const proxyAsLogic1 = await ethers.getContractAt("Logic1", proxy);
+    const proxyAsLogic2 = await ethers.getContractAt("Logic2", proxy);
+
+    return { proxy , logic1, logic2, proxyAsLogic1, proxyAsLogic2 };
   }
   it("Should work with Logic1", async function () {
-    const { proxy , logic1 } = await loadFixture(deployFixture);
+    const { proxy, logic1, proxyAsLogic1 } = await loadFixture(deployFixture);
     await proxy.changeImplementation(logic1);
     assert.equal(await logic1.x(), 0);
 
-    await proxy.changeX(55);
+    await proxyAsLogic1.changeX(55);
     assert.equal(await logic1.x(), 55);
   });
 
-  // it("Should work with upgrades", async function () {
-  //   const { proxy , logic1, logic2 } = await loadFixture(deployFixture);
-  //   await proxy.changeImplementation(logic1);
-  //   assert.equal(await logic1.x(), 0);
+  it("Should work with upgrades", async function () {
+    const { proxy , logic1, logic2, proxyAsLogic1, proxyAsLogic2 } = await loadFixture(deployFixture);
+    await proxy.changeImplementation(logic1);
+    assert.equal(await logic1.x(), 0);
 
-  //   await proxy.changeX(55);
-  //   assert.equal(await logic1.x(), 55);
+    await proxyAsLogic1.changeX(55);
+    assert.equal(await logic1.x(), 55);
 
-  //   await proxy.changeImplementation(logic2);
-  //   assert.equal(await logic2.x(), 0);
+    await proxy.changeImplementation(logic2);
+    assert.equal(await logic2.x(), 0);
 
-  //   await proxy.changeX(50);
-  //   assert.equal(await logic2.x(), 50);
+    await proxyAsLogic2.changeX(50);
+    assert.equal(await logic2.x(), 50);
 
-  //   await proxy.tripleX();
-  //   assert.equal(await logic2.x(), 150);
-  // });
+    await proxyAsLogic2.tripleX();
+    assert.equal(await logic2.x(), 150);
+  });
 });
